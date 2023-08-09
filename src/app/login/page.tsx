380b9735ab24d7b5a49b1398addba2b00268fadc
Andymamooo <andyderkaema@gmail.com>
2023-08-09 07:24:52 -0700

    start login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export default function Login() {
+  const [loginData, setLogin] = useState({
+    email: "",
+    password: "",
+  });
+
+  let onLoginChange = (e) => {
+    e.preventDefault();
+    console.log(e.target.name);
+    setLogin({ ...loginData, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className='flex justify-center items-center h-screen mb-4'>
+      <form className='flex flex-col space-y-4'>
+        <div className='flex flex-col space-y-1'>
+          <label>Email</label>
+          <input
+            name='email'
+            type='email'
+            placeholder='Enter email'
+            value={loginData.email}
+            onChange={onLoginChange}
+            style={{ color: "black", background: "white" }}
+            className='text-black bg-white w-full px-3 py-2 border border-gray-300 rounded w-64'
+          />
+        </div>
+        <div className='flex flex-col space-y-1 '>
+          <label>Password</label>
+          <input
+            name='password'
+            type='text'
+            placeholder='Enter password'
+            value={loginData.password}
+            onChange={onLoginChange}
+            style={{ color: "black", background: "white" }}
+            className='text-black bg-white w-full px-3 py-2 border border-gray-300 rounded w-64'
+          />
+        </div>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+          Login
+        </button>
+        <button>Forgot password?</button>
+        <button>Don't have an account?</button>
+      </form>
+    </div>
+  );
+}
