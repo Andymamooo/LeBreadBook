7531266bc171aa757c6e9f7d8be16334dee34f18
Andymamooo <andyderkaema@gmail.com>
2023-09-13 21:54:52 -0700

    add tailwind css for register page

--- src/app/Register_Page/page.tsx
+++ src/app/Register_Page/page.tsx
@@ -1,14 +1,85 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import Image from "next/image";
 
 export default function Register() {
   return (
-    <div className='flex justify-center items-center h-screen mb-4'>
-      <form className='flex flex-col'>
-        <label>First Name</label>
-        <label>Last Name</label>
-      </form>
+    <div className='bg-gray-50 dark:bg-gray-900'>
+      <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
+        <a
+          href='#'
+          class='flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white'
+        >
+          <Image
+            className='w-8 h-8 mr-2'
+            src='/logo.svg'
+            alt='Logo'
+            height={54}
+            width={54}
+          />
+        </a>
+        <div className='w-full bg-white rounded-lg shadow dark:border sm:max-w-md xl:p-0 dark:bg-gray-800'>
+          <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
+            <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
+              Create a account
+            </h1>
+            <form className='space-y-4 md:space-y-6' action='#'>
+              <div>
+                <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+                  Your email
+                </label>
+                <input
+                  type='email'
+                  name='email'
+                  id='email'
+                  className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  placeholder='name@email.com'
+                ></input>
+              </div>
+              <div>
+                <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+                  Password
+                </label>
+                <input
+                  type='password'
+                  name='password'
+                  id='password'
+                  className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  placeholder='••••••••'
+                ></input>
+              </div>
+              <div>
+                <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+                  Confirm password
+                </label>
+                <input
+                  type='confirm-password'
+                  name='confirm-password'
+                  id='confirm-password'
+                  className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  placeholder='••••••••'
+                ></input>
+              </div>
+              <button
+                type='submit'
+                className='w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
+              >
+                Create an account
+              </button>
+              <p className='text-sm font-light text-gray-500 dark:text-gray-400'>
+                Already have an account?{" "}
+                <a
+                  href='#'
+                  className='font-medium text-primary-600 hover:underline dark:text-primary-500'
+                >
+                  Login here
+                </a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
