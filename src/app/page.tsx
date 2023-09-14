61bd54518c98137e4c427f7e2c6b76e9a5ee9ec4
PikaJoyce <PikaJoyce@users.noreply.github.com>
2023-09-13 21:50:10 -0700

    Remove Recipe List from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 'use client';
 
 import { useEffect } from 'react';
-import RecipeCard from './Recipe_Card/page';
-import RecipeList from './Recipe_List/page';
 
 // Recipe List View
 export default function Home() {
@@ -10,16 +8,5 @@ export default function Home() {
     import('preline');
   }, []);
 
-  return (
-    <main>
-      <div
-        className='max-w-xs bg-white border rounded-md shadow-lg dark:bg-gray-800 dark:border-gray-700'
-        role='alert'
-      >
-        <div className='columns-1 items-center'>
-          <RecipeList />
-        </div>
-      </div>
-    </main>
-  );
+  return <main></main>;
 }
