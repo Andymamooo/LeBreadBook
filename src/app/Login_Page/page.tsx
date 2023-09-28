00c500cbfb99b75ca03d99f4e0d9953333ee020f
Andymamooo <andyderkaema@gmail.com>
2023-09-28 07:43:29 -0700

    add same styling to Login Page

--- src/app/Login_Page/page.tsx
+++ src/app/Login_Page/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 export default function Login() {
@@ -28,41 +29,70 @@ export default function Login() {
   };
 
   return (
-    <div className='flex justify-center items-center h-screen mb-4'>
-      <form className='flex flex-col space-y-4'>
-        <div className='flex flex-col space-y-1'>
-          <label>Email</label>
-          <input
-            name='email'
-            type='email'
-            placeholder='Enter email'
-            value={loginData.email}
-            onChange={onLoginChange}
-            style={{ color: "black", background: "white" }}
-            className='text-black bg-white w-full px-3 py-2 border border-gray-300 rounded'
-          />
-        </div>
-        <div className='flex flex-col space-y-1 '>
-          <label>Password</label>
-          <input
-            name='password'
-            type='text'
-            placeholder='Enter password'
-            value={loginData.password}
-            onChange={onLoginChange}
-            style={{ color: "black", background: "white" }}
-            className='text-black bg-white w-full px-3 py-2 border border-gray-300 rounded'
-          />
-        </div>
-        <button
-          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-          onClick={handleLogin}
+    <div className='bg-gray-50 dark:bg-gray-900'>
+      <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
+        <a
+          href='#'
+          className='flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white'
         >
-          Login
-        </button>
-        <button>Forgot password?</button>
-        <button onClick={handleRegister}>Don't have an account?</button>
-      </form>
+          <Image
+            className='w-8 h-8 mr-2'
+            src='/logo.svg'
+            alt='Logo'
+            height={54}
+            width={54}
+          />
+        </a>
+        <div className='w-full bg-white rounded-lg shadow dark:border sm:max-w-md xl:p-0 dark:bg-gray-800'>
+          <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
+            <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
+              Login
+            </h1>
+            <form className='space-y-4 md:space-y-6' action='#'>
+              <div>
+                <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+                  Email
+                </label>
+                <input
+                  type='email'
+                  name='email'
+                  id='email'
+                  className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  placeholder='name@email.com'
+                ></input>
+              </div>
+              <div>
+                <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+                  Password
+                </label>
+                <input
+                  type='password'
+                  name='password'
+                  id='password'
+                  className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  placeholder='••••••••'
+                ></input>
+              </div>
+              <button
+                type='submit'
+                className='w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
+              >
+                Login
+              </button>
+              <p className='text-sm font-light text-gray-500 dark:text-gray-400'>
+                Don't have an account?{" "}
+                <a
+                  href='#'
+                  className='font-medium text-primary-600 hover:underline dark:text-primary-500'
+                  onClick={handleRegister}
+                >
+                  Create one here
+                </a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
